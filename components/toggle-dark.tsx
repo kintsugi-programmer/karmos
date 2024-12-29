80e13c6e1d2509ac6aad1f8c6b44b7deb0d29346
Siddhant Bali <119037976+kintsugi-programmer@users.noreply.github.com>
2024-12-30 03:53:26 +0530

    ADDED Next14  Nav Shadcn Darkmode Sidebar

---
+++ components/toggle-dark.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Button } from "@/components/ui/button"
+
+export function Switch() {
+  const { theme, setTheme } = useTheme()
+
+  // Toggle between light and dark themes
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
+
+  return (
+    <Button variant="outline" size="icon" onClick={toggleTheme} className="rounded-full">
+      {theme === "dark" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem] transition-all" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
