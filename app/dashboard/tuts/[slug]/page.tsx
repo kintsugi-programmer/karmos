6895443e37a5fb02c69e0063c1ae8a81d261a158
Siddhant Bali <119037976+kintsugi-programmer@users.noreply.github.com>
2024-12-30 17:00:48 +0530

    Data

--- app/dashboard/tuts/[slug]/page.tsx
+++ app/dashboard/tuts/[slug]/page.tsx
@@ -50,7 +50,23 @@ type LinksDictionary = {
         </div>
       );
     } else if (content2) {
-      return <Link href={content2} target="_blank" rel="noopener noreferrer">{content2}</Link>;
+      return (
+        
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <Link href={content2} target="_blank" rel="noopener noreferrer">{content2}</Link>
+        {/* Responsive Iframe */}
+        <div className="flex-1 rounded-2xl bg-muted/50">
+              <iframe
+    src={content2}
+    title="YouTube video player"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerPolicy="strict-origin-when-cross-origin"
+    allowFullScreen
+    className="w-full h-full rounded-2xl aspect-video"
+  ></iframe>
+        </div>
+        </div>
+      );
     } else {
       return <NotFound />;
     }
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -2,139 +2,211 @@
 
 import * as React from "react"
 import {
-  AudioWaveform,
   BookOpen,
   Bot,
   Command,
   Frame,
-  GalleryVerticalEnd,
+  LifeBuoy,
   Map,
   PieChart,
+  Send,
   Settings2,
   SquareTerminal,
 } from "lucide-react"
 
 import { NavMain } from "@/components/nav-main"
 import { NavProjects } from "@/components/nav-projects"
+import { NavSecondary } from "@/components/nav-secondary"
 import { NavUser } from "@/components/nav-user"
-import { TeamSwitcher } from "@/components/team-switcher"
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
-  SidebarRail,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar"
 
-// This is sample data.
 const data = {
   user: {
     name: "shadcn",
     email: "m@example.com",
     avatar: "/avatars/shadcn.jpg",
   },
-  teams: [
-    {
-      name: "Acme Inc",
-      logo: GalleryVerticalEnd,
-      plan: "Enterprise",
-    },
-    {
-      name: "Acme Corp.",
-      logo: AudioWaveform,
-      plan: "Startup",
-    },
-    {
-      name: "Evil Corp.",
-      logo: Command,
-      plan: "Free",
-    },
-  ],
   navMain: [
     {
-      title: "Playground",
+      title: "Mobile Computing",
       url: "#",
       icon: SquareTerminal,
       isActive: true,
       items: [
         {
-          title: "History",
-          url: "#",
+          title: "Java",
+          url: "/dashboard/tuts/java",
         },
         {
-          title: "Starred",
-          url: "#",
+          title: "OOPs",
+          url: "/dashboard/tuts/oops",
         },
         {
-          title: "Settings",
-          url: "#",
+          title: "Android Studio",
+          url: "/dashboard/tuts/android-java",
+        },
+        {
+          title: "Mobile Computing",
+          url: "/dashboard/tuts/mobile-computing",
         },
       ],
     },
     {
-      title: "Models",
-      url: "#",
+      title: "Found. of CyberSec",
+      url: "/dashboard/tuts/foundations-cyber-security",
       icon: Bot,
       items: [
         {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
+          title: "FCS Course",
+          url: "/dashboard/tuts/foundations-cyber-security",
         },
       ],
     },
+   
     {
-      title: "Documentation",
-      url: "#",
+      title: "OTW Wargames",
+      url: "/dashboard/tuts/otw-wargames",
       icon: BookOpen,
       items: [
         {
-          title: "Introduction",
-          url: "#",
+          title: "Bandit",
+          url: "/dashboard/tuts/otw-wargames-bandit",
         },
         {
-          title: "Get Started",
-          url: "#",
+          title: "Bandit Tuts",
+          url: "/dashboard/tuts/otw-bandit",
         },
         {
-          title: "Tutorials",
-          url: "#",
+          title: "Other Wargames",
+          url: "/dashboard/tuts/otw-wargames",
         },
-        {
-          title: "Changelog",
-          url: "#",
-        },
+
       ],
     },
     {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
+      title: "100xDevs",
+      url: "/dashboard/tuts/100xdev-app",
+      icon: BookOpen,
       items: [
         {
-          title: "General",
-          url: "#",
+          title: "FullStack WebDev",
+          url: "/dashboard/tuts/webdev",
         },
         {
-          title: "Team",
-          url: "#",
+          title: "DevOps",
+          url: "/dashboard/tuts/devops",
         },
         {
-          title: "Billing",
-          url: "#",
+          title: "Web3",
+          url: "/dashboard/tuts/web3",
         },
         {
-          title: "Limits",
-          url: "#",
+          title: "Assignments",
+          url: "/dashboard/tuts/100xdev-ass",
         },
+        {
+          title: "Projects",
+          url: "/dashboard/tuts/100xdev-proj",
+        },
+        {
+          title: "App",
+          url: "/dashboard/tuts/100xdev-app",
+        },
+        {
+          title: "Syllabus",
+          url: "/dashboard/tuts/100xdev-syll",
+        },
+        {
+          title: "Github",
+          url: "/dashboard/tuts/code100x",
+        },
+
+
       ],
+    },
+    {
+      title: "CP31",
+      url: "/dashboard/tuts/cp",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Codeforces",
+          url: "/dashboard/tuts/cp",
+        },
+        {
+          title: "Tutorials",
+          url: "/dashboard/tuts/cp31-tuts",
+        },
+        {
+          title: "CP31 Sheet",
+          url: "/dashboard/tuts/cp31",
+        },
+        {
+          title: "800 Tuts",
+          url: "/dashboard/tuts/cp31-800",
+        },
+       
+
+      ],
+    },
+    {
+      title: "DS&A",
+      url: "/dashboard/tuts/dsa",
+      icon: BookOpen,
+      items: [
+        {
+          title: "A2Z Sheet",
+          url: "/dashboard/tuts/dsa-a2z",
+        },
+        {
+          title: "Tutorials",
+          url: "/dashboard/tuts/dsa",
+        },
+        {
+          title: "CP31 Sheet",
+          url: "/dashboard/tuts/cp31",
+        },
+        {
+          title: "800 Tuts",
+          url: "/dashboard/tuts/cp31-800",
+        },
+       
+
+      ],
+    },
+    {
+      title: "Others",
+      url: "/dashboard/tuts/typing",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Touch Typing",
+          url: "/dashboard/tuts/typing",
+        }
+
+       
+
+      ],
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Support",
+      url: "#",
+      icon: LifeBuoy,
+    },
+    {
+      title: "Feedback",
+      url: "#",
+      icon: Send,
     },
   ],
   projects: [
@@ -156,20 +228,160 @@ const data = {
   ],
 }
 
+
+
+// const data = {
+//   user: {
+//     name: "shadcn",
+//     email: "m@example.com",
+//     avatar: "/avatars/shadcn.jpg",
+//   },
+//   navMain: [
+//     {
+//       title: "Playground",
+//       url: "#",
+//       icon: SquareTerminal,
+//       isActive: true,
+//       items: [
+//         {
+//           title: "History",
+//           url: "#",
+//         },
+//         {
+//           title: "Starred",
+//           url: "#",
+//         },
+//         {
+//           title: "Settings",
+//           url: "#",
+//         },
+//       ],
+//     },
+//     {
+//       title: "Models",
+//       url: "#",
+//       icon: Bot,
+//       items: [
+//         {
+//           title: "Genesis",
+//           url: "#",
+//         },
+//         {
+//           title: "Explorer",
+//           url: "#",
+//         },
+//         {
+//           title: "Quantum",
+//           url: "#",
+//         },
+//       ],
+//     },
+//     {
+//       title: "Documentation",
+//       url: "#",
+//       icon: BookOpen,
+//       items: [
+//         {
+//           title: "Introduction",
+//           url: "#",
+//         },
+//         {
+//           title: "Get Started",
+//           url: "#",
+//         },
+//         {
+//           title: "Tutorials",
+//           url: "#",
+//         },
+//         {
+//           title: "Changelog",
+//           url: "#",
+//         },
+//       ],
+//     },
+//     {
+//       title: "Settings",
+//       url: "#",
+//       icon: Settings2,
+//       items: [
+//         {
+//           title: "General",
+//           url: "#",
+//         },
+//         {
+//           title: "Team",
+//           url: "#",
+//         },
+//         {
+//           title: "Billing",
+//           url: "#",
+//         },
+//         {
+//           title: "Limits",
+//           url: "#",
+//         },
+//       ],
+//     },
+//   ],
+//   navSecondary: [
+//     {
+//       title: "Support",
+//       url: "#",
+//       icon: LifeBuoy,
+//     },
+//     {
+//       title: "Feedback",
+//       url: "#",
+//       icon: Send,
+//     },
+//   ],
+//   projects: [
+//     {
+//       name: "Design Engineering",
+//       url: "#",
+//       icon: Frame,
+//     },
+//     {
+//       name: "Sales & Marketing",
+//       url: "#",
+//       icon: PieChart,
+//     },
+//     {
+//       name: "Travel",
+//       url: "#",
+//       icon: Map,
+//     },
+//   ],
+// }
+
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props}>
+    <Sidebar variant="inset" {...props}>
       {/* <SidebarHeader>
-        <TeamSwitcher teams={data.teams} />
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">Acme Inc</span>
+                  <span className="truncate text-xs">Enterprise</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
       </SidebarHeader> */}
       <SidebarContent>
         <NavMain items={data.navMain} />
         <NavProjects projects={data.projects} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
-      <SidebarFooter>
+      {/* <SidebarFooter>
         <NavUser user={data.user} />
-      </SidebarFooter>
-      <SidebarRail />
+      </SidebarFooter> */}
     </Sidebar>
   )
 }
